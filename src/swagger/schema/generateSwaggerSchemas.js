fb0b0c42f4428654126e4236cbf1b44b4cd8d309
Anh Dung(dev) <dungnaph52171@gmail.com>
2025-05-31 22:08:53 +0700

    [feature] update api docs user + [fix] path image

--- src/swagger/schema/generateSwaggerSchemas.js
+++ src/swagger/schema/generateSwaggerSchemas.js
@@ -1,10 +1,11 @@
 import mongooseToSwagger from 'mongoose-to-swagger';
-import {productModel} from '../../models/index.js';
+import {productModel, userModel} from '../../models/index.js';
 import { authSchemas } from './auth.schemas.js';
 
 const generateSchema = (model) => mongooseToSwagger(model);
 
 export const swaggerSchemas = {
   Product: generateSchema(productModel),
+  User: generateSchema(userModel),
   ...authSchemas
 };
